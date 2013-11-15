4e29f776a77949219f48c44ea0fc43bd6560bdee
ihenvyr <ihenvyr@gmail.com>
2013-11-15 12:38:25 +0800

    update.php updated

--- update.php
+++ update.php
@@ -1,5 +1,13 @@
-<?php `git pull origin master`;
-echo "test start";
-$output = shell_exec( 'git pull origin master' );
+<?php //`git pull origin master`;
+// echo "test start";
+// $output = shell_exec( 'git pull origin master' );
+// echo "<pre>$output</pre>";
+// echo "test end";
+
+// using backticks
+$output = `ls -al`;
 echo "<pre>$output</pre>";
-echo "test end";
+
+// using shell_exec
+$output = shell_exec('ls -lart');
+echo "<pre>$output</pre>";
