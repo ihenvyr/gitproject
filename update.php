0a181365eb72b958c9fb601a7377b7193d311bbc
ihenvyr <ihenvyr@gmail.com>
2013-11-15 13:52:05 +0800

    testing update.php

--- update.php
+++ update.php
@@ -1,26 +1,5 @@
 <?php
 
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
-
-// echo "test start";
-// $output = shell_exec( 'git pull origin master' );
-// echo "<pre>$output</pre>";
-// echo "test end";
-
-// // using backticks
-// $output = `ls -al`;
-// echo "<pre>$output</pre>";
-
-// // using shell_exec
-// $output = shell_exec('ls -lart');
-// echo "<pre>$output</pre>";
-
 // git pull using backticks
-$output = `git pull`;
-echo "<pre>$output</pre>";
-
-// git pull using shell_exec
-$output = shell_exec('git pull');
+$output = `sudo git pull`;
 echo "<pre>$output</pre>";
